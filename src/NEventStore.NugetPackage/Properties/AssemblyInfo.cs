e2bb32cb1a64ca434a44f6a04e53936ae6550ee2
Paulius Maciulis <paulius@devbridge.com>
2015-12-16 15:25:02 +0200

    NugetPackage assembly extended with the version file.

--- src/NEventStore.NugetPackage/Properties/AssemblyInfo.cs
+++ src/NEventStore.NugetPackage/Properties/AssemblyInfo.cs
@@ -6,5 +6,3 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("37b1adb7-a468-474e-97a4-7d5165a67b3e")]
-[assembly: AssemblyVersion("10.0.0.0")]
-[assembly: AssemblyFileVersion("10.0.0.0")]
